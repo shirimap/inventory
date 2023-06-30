64feb3213e62dc7df10f0c8a181df54893a88b13
shirimap <shirimap123@gmail.com>
2023-07-01 00:43:47 +0300

    threeshold

--- database/migrations/2022_07_23_191454_create_sbidhaas_table.php
+++ database/migrations/2022_07_23_191454_create_sbidhaas_table.php
@@ -17,6 +17,7 @@ class CreateSbidhaasTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('type');
+            $table->integer('threshold');
             $table->timestamps();
         });
     }
